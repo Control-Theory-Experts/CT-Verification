3699885c245898266339fa78cb5fd95c35421011
sebastianbrandt <sebastian.y.brandt@gmx.de>
2025-02-18 01:42:53 +0100

    update

---
+++ Possible Controller/Implementation/Quadcopter/Quadcopter_skript.m
@@ -0,0 +1,27 @@
+% Masse des Quadcopters 0.5 bis 2kg
+m = 1;
+
+% Trägheitsmomente des Quadcopters
+I_xx = 0.01;
+I_yy = 0.01;
+I_zz = 0.02;
+
+% Erdbeschleunigung
+g = 9.81;
+
+% Abstand zwischen Motor und Schwerpunkt
+L = 0.225;
+
+% Trägheitsmoment der Propeller Blades
+J_r = 5.33 * 10^-5;
+
+% Winkelgeschwindigkeit des Motors
+Omega_d = 995;
+
+% Gesamtschubkraft
+U_1 = 25.72;
+
+% Drehmomente
+U_2 = 0.2;
+U_3 = 0.5;
+U_4 = 0.25;
